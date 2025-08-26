6c45af2671d7652650ed4f6595e1dd3a34cac0d1
Daniel Shin <daniel@netcorp.com.au>
2025-08-26 14:11:09 +1000

    2025-08-26

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
 import {Card, CardContent} from "@/components/ui/card"
 // import App248 from './248_useMemo/App248';
-import App243 from './243_memo/App243';
+// import App243 from './243_memo/App243';
+import App278 from './278_cross_comps/App278.jsx';
+// declare const App278: React.FC;
 
 function App() {
 
   return (
     <Card className='mx-auto max-w-lg mt-10'>
       <CardContent>
-        <App243 />
+        <App278 />
       </CardContent>
     </Card>
   );
---
+++ src/global.d.ts
@@ -0,0 +1,2 @@
+declare const App278: React.FC;
+export default App278;
